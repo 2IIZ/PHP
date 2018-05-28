aa6e9fcf05914574b3923cde180581742de6fd78
Unknown <2IIZ1998@gmail.com>
2018-05-28 17:58:52 +0200

    added a index.php that group all my templates

---
+++ myTemplates/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>My templates</title>
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="master.css">
+  </head>
+  <body>
+    <div class="card" style="width: 18rem;">
+      <img class="card-img-top" src="ConnexionBoostrap/img/img.jpg" alt="Card image cap">
+      <div class="card-body">
+        <h5 class="card-title">Sing In</h5>
+        <p class="card-text">Basic Sing In form</p>
+        <a href="ConnexionBoostrap/index.php" class="btn btn-primary">Go</a>
+      </div>
+    </div>
+
+    <h1 class="welcomeTitle">Welcome</h1>
+
+  </body>
+</html>
